b1e9b7c6a98461c2ff7f382eada58f76165f0de5
Wes <wes@zipcoders-MacBook-Pro.local>
2021-08-15 16:50:53 -0400

    customer_consumer_update

--- customer_consumer.py
+++ customer_consumer.py
@@ -1,14 +1,18 @@
 from kafka import KafkaConsumer, TopicPartition
 from json import loads
+from sys import argv
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.orm import declarative_base
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     engine = create_engine('sqlite:///bank.db', echo = True)
     db = engine.connect()
-    consumer = KafkaConsumer('new_customers',
-            bootstrap_servers=['localhost:9092'],
-            value_deserializer=lambda m: loads(m.decode('ascii')))
+    consumer = KafkaConsumer(
+        bootstrap_servers=['localhost:9092'],
+        value_deserializer=lambda m: loads(m.decode('ascii')))
+
+    partition = int(argv[1])
+    consumer.assign([TopicPartition('new_customers', partition)])
 
     for message in consumer: 
             #print(message.value)
