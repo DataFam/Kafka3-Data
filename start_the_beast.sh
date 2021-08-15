9971c22bb682231228adf6901e050d715a3eb067
Aidan Farhi <aidanmcfarhi@gmail.com>
2021-08-15 18:18:27 -0400

    added script to start everythin and initialize db

---
+++ start_the_beast.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+echo "...Initalizing DB..."
+python3 init_db.py
+echo "...Starting the beast..."
+python3 customer_consumer.py 0 & python3 customer_consumer.py 1 & python3 customer_consumer.py 2 & python3 limit_consumer.py & python3 summary_consumer.py & python3 transaction_con_prod.py & python3 customer_producer.py 0 & python3 customer_producer.py 1 & python3 customer_producer.py 2 & python3 producer-random-xactions.py 0 & python3 producer-random-xactions.py 1 & python3 producer-random-xactions.py 2 
